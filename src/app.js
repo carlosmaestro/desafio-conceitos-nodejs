da589e0ee0cf8f9f95e761ecafdd39a7c3b79e78
C1316161 Carlos Henrique Moreira Maestro <c1316161@interno.bb.com.br>
2020-12-01 19:41:22 -0300

    completing the challenge

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
 
 // const { v4: uuid, validate: isUuid } = require('uuid');
 
@@ -10,24 +11,80 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get('/repositories', (request, response) => {
+  response.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post('/repositories', (request, response) => {
+
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuidv4(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  response.status(200).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+
+  const indexRepository = repositories.findIndex(repository => { return repository.id === id });
+
+  if (indexRepository < 0) {
+    return response.status(400).json({ error: 'repository not found' });
+  }
+
+  const { title, url, techs } = request.body;
+
+  const repository = repositories[indexRepository];
+
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+
+  repositories[indexRepository] = repository;
+
+  return response.json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete('/repositories/:id', (request, response) => {
+
+  const { id } = request.params;
+
+  const indexRepository = repositories.findIndex(repository => { return repository.id === id });
+
+  if (indexRepository < 0) {
+    return response.status(400).json({ error: 'repository not found' });
+  }
+
+  repositories.splice(indexRepository, 1);
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post('/repositories/:id/like', (request, response) => {
+  const { id } = request.params;
+
+  const indexRepository = repositories.findIndex(repository => { return repository.id === id });
+
+  if (indexRepository < 0) {
+    return response.status(400).json({ error: 'repository not found' });
+  }
+
+  const repository = repositories[indexRepository];
+
+  repository.likes++;
+
+  repositories[indexRepository] = repository;
+
+  return response.json(repository);
 });
 
 module.exports = app;
